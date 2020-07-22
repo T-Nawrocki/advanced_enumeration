11a184ee892ab0b4a9c2ea607ce25462f5bd1419
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-22 16:12:58 +0100

    length and case tests pass

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,13 +1,20 @@
 const AnagramFinder = function (word) {
-    this.word = word;
+    this.word = word.toLowerCase();
     this.chars = this.word.split("");
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
     return otherWords.filter(word => {
+        word = word.toLowerCase()
         const chars = word.split("");
 
-        return this.chars.every(char => chars.includes(char));
+        // every char in base word must be in target word
+        let result = this.chars.every(char => chars.includes(char));
+
+        // length of both words must match
+        result = result && this.word.length === word.length;
+
+        return result
     });
 }
 
