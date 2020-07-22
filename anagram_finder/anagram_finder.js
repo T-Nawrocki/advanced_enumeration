aaf2d0bbe883e8e1ef985fe050baeccb0562dbcf
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-22 16:04:30 +0100

    anagram finder passes first two basic tests

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,15 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
+    this.chars = this.word.split("");
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+    return otherWords.filter(word => {
+        const chars = word.split("");
 
+        return this.chars.every(char => chars.includes(char));
+    });
 }
+
 
 module.exports = AnagramFinder;
