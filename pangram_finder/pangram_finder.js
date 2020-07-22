cac297acd5528a8cd246bbdb489a893fc915c5f3
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-22 15:38:12 +0100

    pangram finder passes all tests

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,13 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  phrase = phrase.toLowerCase();  // case insensitive
+  phrase = phrase.split("");  // splits to array of chars
+  phrase = phrase.filter(char => this.alphabet.includes(char));  // no punctuation
+  this.phrase = phrase
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.every(char => this.phrase.includes(char));
 }
 
 module.exports = PangramFinder;
--- pangram_finder/pangram_finder_spec.js
+++ pangram_finder/pangram_finder_spec.js
@@ -4,6 +4,7 @@ const PangramFinder = require('./pangram_finder.js');
 describe('PangramFinder', function () {
   it('should be able to detect a pangram', function () {
     const pangramFinder = new PangramFinder('the quick brown fox jumps over the lazy dog');
+    console.log('pangramFinder.phrase :>> ', pangramFinder.phrase);
     assert.strictEqual(pangramFinder.isPangram(), true);
   });
 
@@ -36,4 +37,5 @@ describe('PangramFinder', function () {
     const pangramFinder = new PangramFinder('');
     assert.strictEqual(pangramFinder.isPangram(), false);
   });
+
 });
