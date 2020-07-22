82fa53ef4ad2d050749890b3d4afb3e06fcf3830
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-22 15:57:37 +0100

    isogram_finder passes all tests

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,16 @@
 const IsogramFinder = function (word) {
-
+    this.word = word.toLowerCase();
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+    const allChars = this.word.split("");
 
+    const uniqueChars = allChars.filter((char, index) => {
+        // indexOf returns index of first found instance
+        return allChars.indexOf(char) === index  
+    });
+
+    return uniqueChars.length === allChars.length;
 }
 
 module.exports = IsogramFinder;
