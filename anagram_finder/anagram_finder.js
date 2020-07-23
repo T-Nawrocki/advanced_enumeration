feca82930cc50917b5fa6bc69069cbf09e7b9579
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-23 09:20:32 +0100

    minor cleanup only

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -8,10 +8,8 @@ AnagramFinder.prototype.findAnagrams = function (otherWords) {
         word = word.toLowerCase()
         const chars = word.split("").sort();
 
-        // every char in base word must be in target word
-        // TODO Replace with checking if chars.sorted are equal
+        // base word chars must match target word chars
         let result = this.chars.every((char, index) => chars[index] === char);
-        // let result = this.chars.every(char => chars.includes(char));
 
         // length of both words must match
         result = result && this.word.length === word.length;
